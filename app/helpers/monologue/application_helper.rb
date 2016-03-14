a7c01514406c5089733aa95bb7d1c3c294ca9661
Brad Huber <daybreaker@gmail.com>
2016-03-13 22:09:13 -0500

    Update application_helper.rb

--- app/helpers/monologue/application_helper.rb
+++ app/helpers/monologue/application_helper.rb
@@ -47,9 +47,16 @@ module Monologue
       request.protocol + request.host + url
     end
 
+    # def social_icon(foundicon, url, setting)
+    #  return if setting.nil? || !setting
+    #  content_tag :a, href: url, class: "social", target: "_blank" do
+    #    content_tag :i, class: "foundicon-#{foundicon}" do # using an empty content tag for foundicons to appear. TODO: try to do otherwise and use only tag method
+    #    end
+    #  end
+    # end
     def social_icon(foundicon, url, setting)
       return if setting.nil? || !setting
-      content_tag :a, href: url, class: "social", target: "_blank" do
+      content_tag :a, href: url, class: "social #{foundicon}", target: "_blank" do
         content_tag :i, class: "foundicon-#{foundicon}" do # using an empty content tag for foundicons to appear. TODO: try to do otherwise and use only tag method
         end
       end
